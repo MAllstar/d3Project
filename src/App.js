425c654901947c105b558de3d6842c3b94cf82f8
丛鱼 <siqing.msq@dtwave-inc.com>
2020-07-31 13:29:05 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
 import React, {useState, useEffect} from 'react'
 
-const delayTime = 3000
+// 可见状态下的延迟
+const visibleDelay = 3000
+// 不可见状态下的延迟
+const invisibleDelay = 1000
+// 触发刷新定时器执行次数
+const timerTimes = 2
 
 const App = () => {
   // 是否触发事件
   const [isEventActive, setIsEventActive] = useState(false)
   // 计时器id
-  const [timerId, setTimerId] = useState(0)
+  const [timerId, setTimerId] = useState(null)
   // 计时器操作序号
   const [timerNumber, setTimerNumber] = useState(1)
-
+  // 延迟时间
+  const [delayTime, setDelayTime] = useState(document.visibilityState === 'hidden' ? invisibleDelay : visibleDelay)
+  const [timestamp, setTimestamp] = useState(new Date().getTime())
   // 触发事件后初始化
-  const initTimer = (timerId, isEventActive, timerNumber) => {
+  const initTimer = () => {
     setIsEventActive(true)
-    clearTimeout(timerId)
+    if (timerId) clearTimeout(timerId)
+    console.log(timerId)
     setTimerNumber(0)
     // 触发事件后的30分钟无操作进入timer初始化
-    const timerInit = setTimeout(() => {
-      if (!isEventActive && timerNumber) {
+    const timer = setTimeout(() => {
+      if (!isEventActive) {
         setIsEventActive(false)
         setTimerNumber(1)
       }
-      clearTimeout(timerInit)
     }, delayTime)
+    console.log(111, timer)
+  }
+  // 定时器顺序执行操作
+  const timerOrder = () => {
+    if (timerNumber !== timerTimes) {
+      console.warn(`第${timerNumber}次无操作, 请尽快操作页面, ${timerTimes}次后将刷新页面`)
+      setTimerNumber(o => o + 1)
+    } else {
+      console.log('刷新')
+      window.location.reload()
+    }
   }
 
   useEffect(() => {
     const isVisible = !(document.visibilityState === 'hidden')
-    // console.log(isVisible, isEventActive, timerNumber)
+    console.log(isVisible, isEventActive, timerNumber)
     const timer = setTimeout(() => {
       // 可见状态下，当前页面2个30分钟无操作
       if (!isEventActive && isVisible && timerNumber) {
-        if (timerNumber === 1) {
-          console.warn('请尽快操作页面，30分钟后重新刷新')
-          // 修改timerNumber为2，重新渲染计时
-          setTimerNumber(2)
-        } else if (timerNumber === 2) window.location.reload()
+        timerOrder()
       }
       // 不可见状态下
       else if (!isVisible) {
-        if (timerNumber === 1) {
-          console.log('不可见状态下事件')
-          // 修改timerNumber为2，重新渲染计时
-          setTimerNumber(2)
-        } else if (timerNumber === 2) window.location.reload()
+        timerOrder()
       }
       // 可见状态下有操作
       else {
@@ -53,7 +63,7 @@ const App = () => {
     }, delayTime)
     // 设置timerId，以触发事件后清除当前计时器
     setTimerId(timer)
-  }, [isEventActive, timerNumber])
+  }, [isEventActive, timerNumber, delayTime])
 
   useEffect(() => {
     // 浏览器切换事件
@@ -61,23 +71,25 @@ const App = () => {
       // 状态判断
       if (document.visibilityState === 'hidden') {
         // document.title = '不可见';
-        initTimer(timerId, isEventActive, timerNumber)
+        setDelayTime(invisibleDelay)
+        initTimer()
       } else {
         // document.title = '可见';
-        initTimer(timerId, isEventActive, timerNumber)
+        setDelayTime(visibleDelay)
+        initTimer()
       }
     })
 
     document.addEventListener('mousedown', () => {
-      initTimer(timerId, isEventActive, timerNumber)
+      initTimer()
     })
     document.addEventListener('mousemove', () => {
-      initTimer(timerId, isEventActive, timerNumber)
+      initTimer()
     })
     document.addEventListener('keypress', () => {
-      initTimer(timerId, isEventActive, timerNumber)
+      initTimer()
     })
-  }, [timerId, isEventActive, timerNumber])
+  })
 
   return (
     <button>test</button>
